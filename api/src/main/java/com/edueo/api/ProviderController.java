4ad5f7daa756a10a3c1486436f7e0a02d26b4907
Eduardo Alencar de Oliveira <ealencar10@gmail.com>
2023-02-13 12:51:34 -0300

    feat: endpoint para criar prestador

--- api/src/main/java/com/edueo/api/ProviderController.java
+++ api/src/main/java/com/edueo/api/ProviderController.java
@@ -1,6 +1,8 @@
 package com.edueo.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,5 +18,10 @@ public class ProviderController {
     @GetMapping("/providers")
     List<Provider> all() {
         return repository.findAll();
+    }
+
+    @PostMapping("/providers")
+    Provider newProvider(@RequestBody Provider newProvider) {
+        return repository.save(newProvider);
     }
 }
