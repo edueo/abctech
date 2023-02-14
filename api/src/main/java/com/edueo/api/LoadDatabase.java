bc49e4e61a7513396db7c06f0ed55b0824703ec0
Eduardo Alencar de Oliveira <ealencar10@gmail.com>
2023-02-14 06:23:09 -0300

    feat: carrega dados dos serviços prestados

---
+++ api/src/main/java/com/edueo/api/LoadDatabase.java
@@ -0,0 +1,20 @@
+package com.edueo.api;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoadDatabase {
+
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+    @Bean
+    CommandLineRunner initDatabase(ServiceRepository repository) {
+        return args -> {
+            log.info("Preloading " + repository.save(new Service("Troca de aparelho", "Troca de aparelho decodificador de sinal")));
+
+        };
+    }
+}
